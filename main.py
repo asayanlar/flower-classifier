e116e777834f180f3bfb612a46758b41e8c27cc1
asayanlar <andrewsayanlar@gmail.com>
2024-03-03 20:16:51 -0500

    fixed file path to the flower data set in the Path function of main,py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os.path
 import pandas as pd
 
-flower_img_dir = Path("data_set/flowers")
+flower_img_dir = Path("dataset/flowers")
 filepaths = list(flower_img_dir.glob("**/*.jpg"))
 labels = list(map(lambda x: os.path.split(os.path.split(x)[0])[1], filepaths))
 
